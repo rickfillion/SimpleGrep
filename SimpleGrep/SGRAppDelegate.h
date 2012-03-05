05de2b55c4f42333e2d15a6906d35646f302fd68
Rick Fillion <rick@centrix.ca>
2012-03-05 18:11:17 -0500

    Initial Commit

---
+++ SimpleGrep/SGRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SGRAppDelegate.h
+//  SimpleGrep
+//
+//  Created by Rick Fillion on 12-03-05.
+//  Copyright (c) 2012 Centrix.ca. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SGRAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ SimpleGrep/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SimpleGrep
+//
+//  Created by Rick Fillion on 12-03-05.
+//  Copyright (c) 2012 Centrix.ca. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
